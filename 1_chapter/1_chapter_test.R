3ba76467c814b95abc3cb12ac55e1fdb20595eac
smam02 <smam02@vse.cz>
2024-02-18 15:06:35 +0100

    Initial commit and 1_chapter test

---
+++ 1_chapter/1_chapter_test.R
@@ -0,0 +1,29 @@
+# 1. printing help
+help("mean")
+?mean
+
+# 2. use cases of dim
+example("dim")
+
+# 3. Demo examples of image
+demo(image)
+
+# 4. Location of the current workspace
+getwd()
+
+# 5. Defining variables
+a="randomString"
+a1=1-i
+b=TRUE
+c=-1.333
+d=Inf
+objects()
+ls(pattern="a")
+rm(c)
+ls()
+
+#6. Which data structure would you use:
+#matrix
+#list
+#vector
+#data table
